08890f5380ba8441c077bffe7b556fe66fdbc274
GmalFari <gmalfari26@gmail.com>
2022-02-09 17:39:04 +0300

    add check_no_network function

--- check_full.py
+++ check_full.py
@@ -1,4 +1,48 @@
 #!/usr/bin/env python3
+import os
+import shutil
+import sys
+import socket
+
+
+
+def check_reboot():
+    """Returns True if the computer has a pending reboot"""
+    return os.path.exists("/run/reboot-required")
+
+def check_disk_full(disk,min_gb,min_percent):
+    """Returns True if there isn't enough disk space , False otherwise"""
+    du = shutil.disk_usage(disk)
+    #Calculate the percentage of free space
+    percent_free = du.free /2**30
+    if gigabytes_free < min_gb or percent_free < min_percent:
+        return True
+    return False
+
+def check_root_full():
+    """Returns True if the root partition is full , False otherwise."""
+    return check_disk_full(disk="/", min_gb=2,min_percent=10)
+
+def check_no_network():
+    try:
+        socket.gethostbyname("www.google.com")
+        return False
+    except:
+        return True
 
 def main():
-    pass
+    checks=[
+            (check_reboot , "Pending Reboot"),
+            (check_root_full,"Root partition full"),
+            (check_no_network,"No working network")
+    ]
+
+    eveything_ok=True
+    for check,msg in checks:
+        if check():
+            print(msg)
+            everything_ok=False
+
+    if not everything_ok:
+        sys.exit(1)
+
